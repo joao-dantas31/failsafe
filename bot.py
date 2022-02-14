d25fd59388bceaf4cb145f84cee1df1e2eea2ce3
João Paulo Dantas <joao.dantas@ccc.ufcg.edu.br>
2022-02-14 16:06:09 -0300

    innitial commit

---
+++ bot.py
@@ -0,0 +1,77 @@
+import discord
+import requests
+import pandas as pd
+import numpy as np
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow,Flow
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+
+client = discord.Client()
+url = "https://stats.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/{idRaid}/"
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+SAMPLE_SPREADSHEET_ID_input = '1feYTKrvdT-PmbkdF-Asj8Cutq8DD4oxxexrRR7IHfP0'
+SAMPLE_RANGE_NAME = 'A1:AA1000'
+
+emoji = '\N{SQUARED OK}'
+
+def write(columnsList):
+    creds = service_account.Credentials.from_service_account_file(
+                'aut.json', scopes=SCOPES)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+                                      range=SAMPLE_RANGE_NAME).execute()
+    values_input = result_input.get('values', [])
+
+    if not values_input and not values_expansion:
+        print('No data found.')
+    else:
+        df = pd.DataFrame(values_input[1:], columns=values_input[0])
+        df = df.apply(pd.to_numeric)
+        df['raidId'] = df['raidId'].astype(str)
+        
+        for column in columnsList.keys():
+            if column not in df:
+                df.loc[:, column] = 0
+                
+        for column in list(df):
+            if column not in columnsList:
+                columnsList[column] = 0
+        df.loc[df.shape[0]] = columnsList
+
+        service.spreadsheets().values().update(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+        valueInputOption='RAW',
+        range=SAMPLE_RANGE_NAME,
+        body=dict(
+            majorDimension='ROWS',
+            values=df.T.reset_index().T.values.tolist())
+        ).execute()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$r'):
+        request = requests.get(url.format(idRaid = message.content[3:]), headers={"x-api-key": "99b51483606642a095f0e76b92653ad8"})
+        data = request.json().get('Response')
+        participantes = data.get('entries')
+        columnsList = {'raidId': message.content[3:]}
+        for user in participantes:
+            if(user.get('values').get('completed').get('basic').get('value')):
+                columnsList[user.get('player').get('destinyUserInfo').get('membershipId')] = 1
+        write(columnsList)
+        await message.add_reaction(emoji)
+
+client.run('OTQxNzc2ODA3MzE1NzcxNDUy.Yga4AA.DRoMbZ6ZMm4vyaKQvzSZfDZJn5M')
